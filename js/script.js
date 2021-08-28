4c1d67821d7ffda6fc5b28237c9afe7788a7fab3
Reanipush <reanipush@icloud.com>
2021-08-28 14:49:48 +0300

    UsloviyaSwitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log(1);
+const num = 57;
+
+switch(num) {
+    case 50 : console.log('wrong');
+    break;
+    case 94 : console.log('Non in this time');
+    break;
+    default: console.log('So good');
+    break;
+}
